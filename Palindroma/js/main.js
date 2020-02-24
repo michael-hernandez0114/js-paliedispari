601085d9da44ecc0cfb37c0c9141d7ee825e0282
Michael Hernandez <gammabeta010@gmail.com>
2020-02-24 22:06:58 +0100

    Updates to palindroma

--- Palindroma/js/main.js
+++ Palindroma/js/main.js
@@ -1,14 +1,40 @@
 var userInput;
 var isPalindrome;
+const regExp = '^[A-Za-z]+$'
 
-userInput = prompt("Please enter a word");
+do {
+    userInput = prompt("Scrivi una parola");
+} while (checkUserInput(userInput));
+
+//userInput = prompt("Please enter a word");
 
 isPalindrome = checkPalindrome(userInput)
 
-console.log(userInput[0]);
-console.log(userInput.length);
+//console.log(userInput[0]);
+//console.log(userInput.length);
 
 console.log(isPalindrome);
+
+if (isPalindrome) {
+    alert("La parola che hai scelto e una palindroma!")
+}
+else {
+    alert("La parola che hai scelto non e una palindroma!")
+}
+
+
+function checkUserInput (value) {
+    var keepPrompting;
+
+    if (value.match(regExp) === null) {
+        keepPrompting = true;
+    }
+    else {
+        keepPrompting = false;
+    }
+    console.log(value.match(regExp));
+    return keepPrompting;
+}
 
 function checkPalindrome (word) {
 
